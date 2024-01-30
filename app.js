f15b84873366c4c73fbaa03c11805f1d044a1956
Mark Dexter Abuyabor <a.markdexter@gmail.com>
2024-01-30 10:19:23 +0800

    add: data and templates

---
+++ app.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({
+    // template: '<h2>This is the main template</h2>'
+    data() {
+        return {
+            title: 'Final Empire'
+        }
+    }
+})
+
+app.mount("#app")
