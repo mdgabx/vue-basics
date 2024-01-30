4191aa61e73a0bf96fb8418b95fb5578a1fbe181
Mark Dexter Abuyabor <a.markdexter@gmail.com>
2024-01-30 14:41:50 +0800

    add: events

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const app = Vue.createApp({
     // template: '<h2>This is the main template</h2>'
     data() {
         return {
-            title: 'Legend of Drizzt',
-            author: 'RA Salvadore',
-            age: 38,
-            showBooks: true
+            url: 'https://wdbsa.netlify.app',
+            showBooks: true,
+            x: 0,
+            y: 0,
+            books: [
+                { title: 'book 1', author: 'test', age: 22, isFav: true },
+                {  title: 'Legend of Drizzt', author: 'RA Salvadore', age: 38,isFav: false }
+            ]
         }
     },
     methods: {
@@ -13,12 +17,24 @@ const app = Vue.createApp({
         //     console.log('click')
         //     this.age++
         // }
-
+        handleEvents(e, data) {
+            console.log('mouse', e)
+            if(data) {
+                console.log('data', data)
+            }
+        },
         toggle() {
             this.showBooks = !this.showBooks
         },
         changeTitle(title) {
             this.title = title
+        },
+        handleMousemove(e) {
+            this.x = e.offsetX
+            this.y = e.offsetY
+        },
+        toggleFav(book) {
+            book.isFav = !book.isFav
         }
     }
 })
